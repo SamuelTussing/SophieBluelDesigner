89bac29b4cb92510919aca6512faa5908390a5bd
Tussing Samuel <sml.tussing@gmail.com>
2023-06-05 14:57:35 +0200

    boutons filtres fonctionnels, le portfolio se met a jour au clic. Il reste néanmoins à créer les boutons dynamiquement

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -16,7 +16,7 @@ for (i=0; i<data.length; i++){
 
     //création des éléments image et assignation du contenu dynamique via l'API
     const card = document.createElement("figure")
-    card.setAttribute("data-category",data[i].category.name)
+    card.setAttribute("id",data[i].category.name)
     const image = document.createElement("img")
     image.src = data[i].imageUrl
     const imgTitle = document.createElement("figcaption")
@@ -50,27 +50,47 @@ for (i=0; i<nbrCategory;i++){
     const button= document.querySelector('.filterSection')
 
     const buttonDiv= document.createElement("li")
-    buttonDiv.className = "filterSection_filter"
-    buttonDiv.setAttribute("data-filter",data[i].category.name)
+    //buttonDiv.className = "filterSection_filter"
+    //buttonDiv.setAttribute("data-filter",data[i].category.name)
     buttonDiv.innerHTML = data[i].category.name
 
     button.appendChild(buttonDiv)
 
 }
 
-data.forEach(function(index,array){
-    console.log(index,array)
-    console.log(data[0].category.name)
+//filtre au clique sur les boutons dédié
 
-    
-})
+//on récupère une référence vers les boutons et vers les articles
+const liItem = document.querySelectorAll('ul li');
+const imgItem = document.querySelectorAll('.product figure');
 
-//on essaye de filtrer au clic
+    //pour chaque clique sur un bouton "filtre"
+    liItem.forEach(li => {
+        li.addEventListener("click",function(){
+            //on ajoute la class active au bouton cliqué et on le retire aux autres
+            //on récupère la valeur du filtre avec "value"
+            let value = li.textContent
+            liItem.forEach(li => {
+                li.className = '';
+            });
+            li.className = 'active';
+        
+            //on passe chaque réf en revue et on compare son id avec la valeur du bouton filtre
+            // si valeurs identiques alors l'article reste visible (block)
+            //si valeur différentes alors article invisible (none + scale(0)
+            for(i=0;i<data.length;i++){
+                
+                if(imgItem[i].getAttribute('id') == value || value == "Tous"){
+                    imgItem[i].style.transform = 'scale(1)';
+                    imgItem[i].style.display = 'block';
+                }else{
+                    imgItem[i].style.transform = 'scale(0)';
+                    imgItem[i].style.display = 'none';
+                }
+            }
 
-let indicator = document.querySelector('.indicator').children;
-let main = document.querySelector('.items').children;
-
-
+        }
+    )})
 
 })
       }
